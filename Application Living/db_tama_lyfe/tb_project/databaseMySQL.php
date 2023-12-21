84b2815599d18d37cfb5a757bc79f2e85fb06551
Aura Choirun Nisa <120537279+Auraachn@users.noreply.github.com>
2023-12-21 09:12:28 +0700

    Delete Application Living directory

--- Application Living/db_tama_lyfe/tb_project/databaseMySQL.php
+++
@@ -1,9 +0,0 @@
- <?php
- define('databaseMySQLHost', 'localhost');
- define('databaseMySQLUser', 'id21485821_du_tama_lyfe');
- define('databaseMySQLPassword', '');
- define('databaseMySQLName', 'id21485821_dn_tama_lyfe');
-
- $con = mysqli_connect(databaseMySQLHost, databaseMySQLUser, databaseMySQLPassword, databaseMySQLName) 
- or die('Unable to Connect');
- ?>
